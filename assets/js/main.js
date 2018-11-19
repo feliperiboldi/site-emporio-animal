5608d7a9ecb08a7fb789aa5145832671b1a99f82
Felipe Riboldi <feliperiboldimarques@outlook.com>
2018-11-19 16:12:41 -0200

    Finalização da Estrutura do Site

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let $doc = $('html, body');
+$('.nav-link').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+if($(window).width() > 991) {
+	$('.img-banner').height($(window).height() - 56);
+    $(window).resize(function () {
+        $('.img-banner').height($(window).height() - 56);
+    });
+    } else {
+    $('.img-banner').height($(window).height() - 56);
+    $(window).resize(function () {
+        $('.img-banner').height($(window).height() - 56);
+    });
+}
